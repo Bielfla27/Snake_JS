637dfa5ec1b402e76ec6378d68574ba1f75f3bcf
Bielfla27 <gabibielmft@outlook.com>
2022-10-17 21:55:23 -0300

    Mecânica do jogo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     canvas = document.getElementById("canvas");
-    ctx = canvas.getElementById("2d");
+    ctx = canvas.getContext("2d");
 
     //variaveis
     snake = []
@@ -12,4 +12,26 @@ window.onload = function(){
     velY = 0; 
     grid = 20; 
     tam = 3; //tamanho da cobrinha ao iniciar
+
+    //chamada dda função jogo a cada 100 milisegundos
+    setInterval(jogo, 100)
+
+}
+
+
+function jogo(){
+    //Configuração da tela
+    ctx.fillStyle = "#2980B9"
+    //parêmatros -> distancia borda horizontal ,vertical, largura e altura
+    ctx.fillRect(0,0, canvas.width, canvas.height)  
+
+    //Posicionando a cobra
+    snake.push({x: positionX, y: positionY})
+
+    //configurando a cobra e movimento
+    ctx.fillStyle = "#00f102";
+    for(let i=0; i < snake.length; i++){
+        ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1 )
+    }
+
 }
