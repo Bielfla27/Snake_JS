6104c3777eef77f2917d511891cb52b19ebd478b
Bielfla27 <gabibielmft@outlook.com>
2022-10-17 22:32:49 -0300

    Movimentação da cobra

--- script.js
+++ script.js
@@ -8,13 +8,39 @@ window.onload = function(){
     positionY = 10;
     foodX = 15;  //posicao da comida
     foodY = 15;
-    velX = 0;  //velocidade
+    velX = 0;  //direção que a cobra vai
     velY = 0; 
     grid = 20; 
     tam = 3; //tamanho da cobrinha ao iniciar
 
     //chamada dda função jogo a cada 100 milisegundos
     setInterval(jogo, 100)
+
+    //controles
+    document.addEventListener("keydown", function(e) {
+            switch (e.keyCode) {
+                //seta direita
+                case 39:
+                    velX = 1;
+                    velY = 0;
+                    break;
+                 //seta esquerda 
+                case 37:
+                    velX = -1;
+                    velY = 0;
+                    break;
+                //seta cima
+                case 38:
+                    velX = 0;
+                    velY = -1;
+                    break;
+                //seta baixo
+                case 40:
+                    velX = 0;
+                    velY = 1;
+                    break;
+            }
+    });
 
 }
 
@@ -25,13 +51,37 @@ function jogo(){
     //parêmatros -> distancia borda horizontal ,vertical, largura e altura
     ctx.fillRect(0,0, canvas.width, canvas.height)  
 
+    //deslocamento da cobra
+    positionX += velX;
+    positionY += velY;
+
+    //Espelhamento
+    if(positionX < 0){
+        positionX = grid;
+    }
+    if(positionX > grid){
+        positionX = 0;
+    }
+    if(positionY < 0){
+        positionY = grid;
+    }
+    if(positionY > grid){
+        positionY = 0;
+    }
+
     //Posicionando a cobra
     snake.push({x: positionX, y: positionY})
 
-    //configurando a cobra e movimento
+    //espelhamento
+
+    //configurando a cobra 
     ctx.fillStyle = "#00f102";
     for(let i=0; i < snake.length; i++){
         ctx.fillRect(snake[i].x*grid, snake[i].y*grid, grid-1, grid-1 )
     }
 
+    //Apagando
+    while(snake.length > tam){
+        snake.shift();
+    }
 }
